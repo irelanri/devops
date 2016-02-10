69b2aafef202d8a16bd61bd67987f26f2474f746
Richard Ireland <922775@uk.ema.ad.pwcinternal.com>
2016-02-10 13:27:51 +0000

    Created 2 encrypted data bag items

--- vagrant_getting_started/cookbooks/awesome_customers/attributes/default.rb
+++ vagrant_getting_started/cookbooks/awesome_customers/attributes/default.rb
@@ -2,3 +2,5 @@ default['awesome_customers']['user'] = 'web_admin'
 default['awesome_customers']['group'] = 'web_admin'
 
 default['awesome_customers']['document_root'] = '/var/www/customers/public_html'
+
+default['awesome_customers']['passwords']['secret_path'] = '/vagrant/encrypted_data_bag_secret'
