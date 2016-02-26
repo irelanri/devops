046eeeb077e37be8b98810ed288e4865e6a9e516
Richard Ireland <922775@uk.ema.ad.pwcinternal.com>
2016-02-26 16:56:09 +0000

    Full web app now working on Windows with chef-solo

--- vagrant_getting_started/cookbooks/awesome_customers/recipes/webserver.rb
+++ vagrant_getting_started/cookbooks/awesome_customers/recipes/webserver.rb
@@ -26,9 +26,37 @@ end
 # Write a default home page.
 # I changed this from index.php, since I have not yet enabled php 
 # 
-file "#{node['awesome_customers']['document_root']}/index.html" do
-  content '<html>This is a placeholder</html>'
+
+#
+# Load the secrets file and the encrypted data bag item that holds the root password.
+# Added during PHP section of lesson
+#
+password_secret = Chef::EncryptedDataBagItem.load_secret(node['awesome_customers']['passwords']['secret_path'])
+password_data_bag_item = Chef::EncryptedDataBagItem.load('passwords', 'sql_server_root_password', password_secret)
+
+#
+# Replace file with template so we can drop in DB connection string etc
+#
+#file "#{node['awesome_customers']['document_root']}/index.html" do
+#  content '<html>This is a placeholder</html>'
+#
+template "#{node['awesome_customers']['document_root']}/index.php" do
+  source 'index.php.erb'
   mode '0644'
   owner node['awesome_customers']['user']
   group node['awesome_customers']['group']
+  variables(
+      :database_password => password_data_bag_item['password']
+  )
+end
+
+# Install the mod_php5 Apache module.
+httpd_module 'php5' do
+  instance 'customers'
+end
+
+# Install php5-mysql.
+package 'php5-mysql' do
+  action :install
+  notifies :restart, 'httpd_service[customers]'
 end
