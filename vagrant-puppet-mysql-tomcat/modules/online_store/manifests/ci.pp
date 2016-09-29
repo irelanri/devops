5168284bbee732a7e10e49102ada85012957ed43
Richard Ireland <richard@reefsolutions.co.uk>
2016-09-29 10:24:49 +0100

    Fixed the codehaus repo loss issue by supplying a settings.xml for the ci server

--- vagrant-puppet-mysql-tomcat/modules/online_store/manifests/ci.pp
+++ vagrant-puppet-mysql-tomcat/modules/online_store/manifests/ci.pp
@@ -49,6 +49,23 @@ class online_store::ci {
 		'/var/lib/jenkins/jobs/loja-virtual-devops'
 	]
 
+	#### This section added to overcome Maven repo changes ####
+	$maven_repository_alias_structure = '/var/lib/jenkins/.m2'
+	file { $maven_repository_alias_structure:
+		ensure => 'directory',
+		owner => 'jenkins',
+		group => 'jenkins',
+		require => Class['jenkins::package'],
+	}
+	file { "${maven_repository_alias_structure}/settings.xml":
+		mode => 0644,
+		owner => 'jenkins',
+		group => 'jenkins',
+		source => 'puppet:///modules/online_store/settings.xml',
+		require => File[$maven_repository_alias_structure],
+	}
+	#### End of section ####
+
 	$git_repository = 'https://github.com/irelanri/loja-virtual-devops.git'
 	$git_poll_interval = '* * * * *'
 	$maven_goal = 'install'
