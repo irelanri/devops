7cc57387d7574563243cbf089712fa89c5d34b8d
Richard Ireland <richard@reefsolutions.co.uk>
2017-10-05 16:33:12 +0100

    Wrote a simple MySQL provider for Terraform

--- terraform/example.tf
+++ terraform/example.tf
@@ -1,5 +1,7 @@
 variable "access_key" {}
 variable "secret_key" {}
+variable "rootuser" {}
+variable "rootpasswd" {}
 
 provider "aws" {
   access_key = "${var.access_key}"
@@ -16,4 +18,25 @@ resource "aws_instance" "terraform-example" {
 # Elastic IP
 resource "aws_eip" "ip" {
   instance = "${aws_instance.terraform-example.id}"
+}
+
+# Create a database server
+resource "aws_db_instance" "default" {
+  engine         = "mysql"
+  engine_version = "5.6.35"
+  instance_class = "db.t2.micro"
+  name           = "initial_db"
+  username       = "${var.rootuser}"
+  password       = "${var.rootpasswd}"
+  allocated_storage = 5 #Gb
+
+  # etc, etc; see aws_db_instance docs for more
+}
+
+# Configure the MySQL provider based on the outcome of
+# creating the aws_db_instance.
+provider "mysql" {
+  endpoint = "${aws_db_instance.default.endpoint}"
+  username = "${aws_db_instance.default.username}"
+  password = "${aws_db_instance.default.password}"
 }
